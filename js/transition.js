d113271bb784e0bdeb143ae1465eb453341f6b4d
Gift-Jackson <113178450+Gift-Jackson@users.noreply.github.com>
2024-02-23 10:02:51 +0100

    Update transition.js

--- js/transition.js
+++ js/transition.js
@@ -1,6 +1,6 @@
 window.onload = () => {
     const transition_el = document.querySelector(".transition");
-    const anchors = document.querySelectorAll("nav ul a")
+    const anchors = document.querySelectorAll("a")
   
     setTimeout(() => {
       transition_el.classList.remove("active")
